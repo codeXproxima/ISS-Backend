520d8a3c5c8ac40f8ff7e1b1188a3fa12c1b2d5f
YahiyaSTC <yahiyastc@gmail.com>
2024-05-19 05:23:30 +0500

    ready for deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const DB = require('./config/db');
 const app = express();
 
 const { PORT, env } = process.env;
@@ -11,14 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+
 if (env === "Prod") {
-    console.log = () => {};
-  }
+  console.log = () => {};
+}
 
-app.get("/", (req,res) =>{
-    res.send("working")
-})
+DB();
 
-app.listen(port, () => {
-  console.log(`server is listing on port: http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.send("working");
 });
+
+if (env === "Dev") {
+  app.listen(port, () => {
+    console.log(`server is listing on port: http://localhost:${port}`);
+  });
+} else {
+  app.listen();
+}
